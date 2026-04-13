90c816d46ddbe26358444b03f5277aa992da4dd6
JovanaBugarcicIT4 <jovanabugarcic05@gmail.com>
2026-04-14 00:41:33 +0200

    Izmenjen LinkContainer u Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,26 +2,29 @@ import React from 'react'
 import {Navbar, Container, Nav} from 'react-bootstrap'
 import {FaShoppingCart, FaUser } from 'react-icons/fa'
 import logo from '../assets/logo.png'
+import {LinkContainer} from 'react-router-bootstrap'
 
 const Header = () => {
   return (
    <header>
     <Navbar bg="primary" variant="dark" expand="md" collapseOnSelect>
         <Container>
-            <Navbar.Brand href="/">
+            <LinkContainer to="/">
+            <Navbar.Brand>
             <img src={logo} alt="MyWeddingDress logo" width="30" height="30" className="d-inline-block align-top me-2"></img>
             <span className="fw-bold">MyWeddingDress </span>
             <i>Prodavnica venčanica u Novom Sadu</i>
             </Navbar.Brand>
+            </LinkContainer>
             <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ms-auto">
-                <Nav.Link href="/cart">
+                <LinkContainer to="/cart">
                 <FaShoppingCart/> Korpa
-                </Nav.Link>
-                <Nav.Link href="/login">
+                </LinkContainer>
+                <LinkContainer to="/login">
                 <FaUser/> Prijava
-                </Nav.Link>
+                </LinkContainer>
                 </Nav>
             </Navbar.Collapse>
         </Container>   
